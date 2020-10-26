bd0e1bec3c9c1d4fe72a7c7819256c02d2ca01b2
Jack Tolley <Jack@JackTolley.com>
2020-10-26 18:10:22 +0000

    FIX This

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,6 @@ class MyApp extends App {
     return (
       <Provider
         config={{
-          // @ts-ignore APIKEY located in next.config.js
           apiKey: API_KEY,
           shopOrigin: shopOrigin,
           forceRedirect: true,
