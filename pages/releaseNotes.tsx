c0ec95a25caca93031daaf19e5e5b5b0b9e7b756
Jack Tolley <Jack@JackTolley.com>
2020-12-18 20:43:01 +0000

    Add: Release Notes

---
+++ pages/releaseNotes.tsx
@@ -0,0 +1,25 @@
+import { TitleBar } from "@shopify/app-bridge-react";
+import { Card, Layout, List, Page } from "@shopify/polaris";
+import React from "react";
+
+const ReleaseNotes: React.FC = () => {
+  return (
+    <Page>
+      <TitleBar title="Release Notes"></TitleBar>
+      <Card title="December Update" sectioned>
+        <List>
+          <List.Item>Added Release Notes Section</List.Item>
+          <List.Item>Added Introduction Tutorial Video</List.Item>
+          <List.Item>
+            Added Customization Options for the Accessory Bar
+          </List.Item>
+          <List.Item>
+            Added Classes and Tags to Accessory Bar for css styling
+          </List.Item>
+        </List>
+      </Card>
+    </Page>
+  );
+};
+
+export default ReleaseNotes;
